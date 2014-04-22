af7bf9d21ee707811b8eb5036affc89d3629e4f4
Paul Young <paulyoungonline@gmail.com>
2014-04-22 00:50:05 -0400

    Add implementation for initializing a new keyed archive.

--- lib/keyed_archive.rb
+++ lib/keyed_archive.rb
@@ -1,5 +1,18 @@
+require 'cfpropertylist'
+
+require "keyed_archive/unpacked_objects"
 require "keyed_archive/version"
 
-module KeyedArchive
-  # Your code goes here...
+class KeyedArchive
+  attr_accessor :archiver, :objects, :top, :version
+
+  def initialize(file)
+    plist = CFPropertyList::List.new(:file => file)
+    data = CFPropertyList.native_types(plist.value)
+
+    @archiver = data['$archiver']
+    @objects = data['$objects']
+    @top = data['$top']
+    @version = data['$version']
+  end
 end
