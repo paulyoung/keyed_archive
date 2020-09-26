46566f56d27e4910d8eb1b468d1f38b2e9880fb5
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2020-09-26 10:27:10 -0400

    Remove unpacked_objects which didn't appear to work, replaced with unpacked_top which recursively replaces the values found in the '' Array with their value from the objects array.

--- lib/keyed_archive/unpacked_objects.rb
+++ lib/keyed_archive/unpacked_objects.rb
@@ -1,31 +1,70 @@
 class KeyedArchive
-  def unpacked_objects
-    objects = @objects.clone
-    unpacked_objects = []
 
-    objects.each_with_index do |object, index|
-      if object.is_a? Hash
-        object.each_pair do |key, value|
-          if value.is_a? Hash
-            uid = value['CF$UID']
+  # Loops through the entries within '$top' 
+  # to replace any values that are pointers to objects.
+  def unpacked_top
 
-            if uid
-              new_object = {}
-              new_object[key] = @objects[uid]
-              objects.push new_object
-              unpacked_objects.delete_at(index - 1)
-            end
-          else
-            new_object = {}
-            new_object[key] = value
-            unpacked_objects.push new_object
-          end
-        end
+    # Create the return value
+    unpacked_top = Hash.new
+
+    # Loop over each pair in the '$top' hash
+    # to recursively replace the values
+    @top.each_pair do |key, value|
+      unpacked_top[key] = recursive_replace(value)
+    end
+
+    # Politely return, our job is done
+    return unpacked_top
+  end
+
+  # Handles the recursive replacement of values within the 
+  # '$objects' array. 
+  def recursive_replace(value)
+    # By default we just return the value itself, usually a String
+    to_return = value
+
+    # If we have an Integer, we want to bring in the object 
+    # that Integer points to
+    if value.is_a? Integer
+
+      # If the object the Integer points to is a Hash, 
+      # then we want to recursively replace it
+      if @objects[value].is_a? Hash
+        to_return = recursive_replace(@objects[value])
       else
-        unpacked_objects.push object
+
+        # If what it points to is not a Hash, just return that
+        to_return = @objects[value]
+      end
+
+    # If we have a Hash, we want to check each entry 
+    # and replace any values which need it
+    elsif value.is_a? Hash
+
+      # Build up a new Hash
+      to_return = Hash.new
+
+      # Loop over the pairs to check the key, value, or both
+      value.each_pair do |tmp_key, tmp_value|
+
+        # If this points to an array of objects, 
+        # then we should bring those values in
+        if tmp_key == "NS.objects"
+          new_array = Array.new
+          tmp_value.each do |entry|
+            new_array.push(recursive_replace(entry))
+          end
+          to_return[tmp_key] = new_array
+
+        # Otherwise, we just want to replace the value with
+        # its recursive version
+        else
+          to_return[tmp_key] = recursive_replace(tmp_value)
+        end
       end
     end
 
-    return unpacked_objects
+    # Give back the value, politely
+    return to_return
   end
 end
