11eb0b89a5c6feb92a583f7151cccfcfb6746045
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2020-09-26 15:32:54 -0400

    Update unpacked_top to not hard code as much and watch for infinite loops progrmatically

--- lib/keyed_archive/unpacked_objects.rb
+++ lib/keyed_archive/unpacked_objects.rb
@@ -10,19 +10,23 @@ class KeyedArchive
     # Loop over each pair in the '$top' hash
     # to recursively replace the values
     @top.each_pair do |key, value|
-      unpacked_top[key] = recursive_replace(value)
+      unpacked_top[key] = recursive_replace(value, -1, [])
     end
 
     # Politely return, our job is done
     return unpacked_top
   end
 
+  private
+
   # Handles the recursive replacement of values within the 
-  # '$objects' array. 
-  def recursive_replace(value)
+  # '$objects' array. Tracks the object locations it has 
+  # already touched to prevent infinite loops. 
+  def recursive_replace(value, current_location, locations)
     # By default we just return the value itself, usually a String
     to_return = value
 
+    # If the value is really representing nil, change it to nil
     if value.is_a? String and value == "$null"
       to_return = nil
     end
@@ -31,7 +35,12 @@ class KeyedArchive
     # that Integer points to
     if value.is_a? Integer
 
-      to_return = recursive_replace(@objects[value])
+      # If we ever find a reference to one of our parents, just stop where we are
+      if locations.include?(current_location)
+        to_return = value
+      else
+        to_return = recursive_replace(@objects[value], value, locations.clone.push(current_location)) 
+      end
 
     # If we have a Hash, we want to check each entry 
     # and replace any values which need it
@@ -45,21 +54,17 @@ class KeyedArchive
 
         # If this points to an array of objects, 
         # then we should bring those values in
-        if tmp_key == "NS.objects"
+        if tmp_key == "NS.objects" or tmp_key == "NS.keys"
           new_array = Array.new
           tmp_value.each do |entry|
-            new_array.push(recursive_replace(entry))
+            new_array.push(recursive_replace(entry, entry, locations.clone.push(current_location)))
           end
           to_return[tmp_key] = new_array
-
-        # Handle an edge case in kCLPlacemarkCodingKeyRegion objects
-        elsif tmp_key == "reserved"
-          to_return[tmp_key] = tmp_value
 
         # Otherwise, we just want to replace the value with
         # its recursive version
         else
-          to_return[tmp_key] = recursive_replace(tmp_value)
+          to_return[tmp_key] = recursive_replace(tmp_value, tmp_value, locations.clone.push(current_location))
         end
       end
     end
